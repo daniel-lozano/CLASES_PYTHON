a22f5224141dded8a010f431965407e1c2e5db16
Daniel Lozano Gomez <d.lozano343@colabi2.ad.uniandes.edu.co>
2018-09-19 12:12:23 -0500

    sode solved

--- CPP/grafica.py
+++ CPP/grafica.py
@@ -15,8 +15,10 @@ for i in range(1,len(lineas)):
     y.append(float(yp))
     y1.append(float(yp1))
 
-plt.plot(x,y)
-plt.plot(x,y1,"--")
+plt.plot(x,y,label="$ y(x) $")
+plt.plot(x,y1,"--",label="$ y^{\\prime}(x) $")
 plt.xlabel("$ x $")
 plt.ylabel("$ F(x) $")
+plt.legend()
+plt.savefig("second_derivative.png")
 plt.show()
