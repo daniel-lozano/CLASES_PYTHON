0f2bd718e2d7e669407180c64aa68a8977fbd09f
daniel-lozano <d.lozano343@uniandes.edu.co>
2018-09-18 21:50:51 -0500

    RK4

--- CPP/edo.cpp
+++ CPP/edo.cpp
@@ -18,31 +18,41 @@ void sine_func( double x[], double f[], int N);
 int main() {
     
     int N=10000;
-    double h=1.0/N;
-    double x[N],y[N];
-    double y0=0;
+    double h=2.0*PI/N;
+    double x[N],y[N],y1[N];
+    double y0=1;
+    double k1,k2,k3,k4;
     int i;
     for(i=0;i<N;i++)x[i]=h*i;
     
     y[0]=y0;
+    y1[0]=y0;
     //Solucionando la ecuacion
-    
+    //f'(x)=f(x)=-sin(x)
     for(i=1;i<N;i++){
-        y[i]=y[i-1]+h*exp(x[i]);
+        y[i]=y[i-1]-h*sin(x[i]);
+        
+        k1=-h*sin(x[i]);
+        k2=-h*sin(x[i]+h/2);
+        k3=-h*sin(x[i]+h/2);
+        k4=-h*sin(x[i]+h);
+        y1[i]=(1.0/6)*(k1+2*k2+2*k3+k4)+y1[i-1];
+        
+        
     }
     //imprimiendo en un archivo de salida
     
     ofstream file;
     file.open("solution.txt");
-    file << "x " << "y" << endl;
+    file << "x " << "y "<< "y1" << endl;
     for(i=0;i<N;i++){
-    file << x[i] <<" " << y[i] <<  endl;
+    file << x[i] <<" " << y[i] << " "<< y1[i]<< endl;
     
     }
     
     
     file.close();
-    //f'(x)=f(x)=e^x
+    
     return 0;
 }
 
--- CPP/grafica.py
+++ CPP/grafica.py
@@ -4,15 +4,19 @@ FILE=open("solution.txt","r")
 lineas=FILE.readlines()
 x=[]
 y=[]
+y1=[]
 
 for i in range(1,len(lineas)):
     
     xp=lineas[i].split()[0]
     yp=lineas[i].split()[1]
+    yp1=lineas[i].split()[2]
     x.append(float(xp))
     y.append(float(yp))
+    y1.append(float(yp1))
 
 plt.plot(x,y)
+plt.plot(x,y1,"--")
 plt.xlabel("$ x $")
 plt.ylabel("$ F(x) $")
 plt.show()
